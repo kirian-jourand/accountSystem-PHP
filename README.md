eb8ab6e181b161e95aabd3b042785239889f5651
kirian <kirian.jourand@gmail.com>
2026-05-22 09:08:46 +0200

    ➕ : TailwindCSS

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Petit projet sur le system de sign up et login en PHP. <br>
 tuto suivie : https://youtu.be/Ojk70Ag8Ofs?si=pGWRmAV59L3C64Z4
+
+Le projet dispose d'une dépendance : TailwindCSS
+
+### INSTALLATION
+
+1. ```npm install```
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/output.css" rel="stylesheet">
     <title>Account System - Home</title>
 </head>
 <body>
