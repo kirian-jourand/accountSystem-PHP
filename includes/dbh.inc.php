2bea6aeecc60faf438eed3b9f202aaeee036bfca
kirian <kirian.jourand@gmail.com>
2026-05-27 14:39:07 +0200

    🎨 : changement des variables de connexion à la DB en constante

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,6 +3,11 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
+define("DB_HOST", $_ENV['DB_HOST']);
+define("DB_PORT", $_ENV['DB_PORT']);
+define("DB_NAME", $_ENV['DB_NAME']);
+define("DB_USER", $_ENV['DB_USER']);
+define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
 
 $dbHost = $_ENV['DB_HOST'];
 $dbPort = $_ENV['DB_PORT'];
