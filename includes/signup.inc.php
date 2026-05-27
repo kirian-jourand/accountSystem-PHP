65075aed30a6ebb1c9e50a6a2a8a1a98434b2371
kirian <kirian.jourand@gmail.com>
2026-05-27 17:49:04 +0200

    🥅 : logique pour tester différent règle pour le form signup. Retour à index.php s'il y a une erreur + détail erreur dans la session

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -3,5 +3,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $email = $_POST["e-mail"];
     $pwd = $_POST["pwd"];
-    $confimPwd = $_POST["confirmPwd"];
+    $confimPwd = $_POST["confimPwd"];
+
+    try {
+        require_once "dbh.inc.php";
+        require_once "signup_model.inc.php";
+        require_once "signup_contr.inc.php";
+
+        // Error handler
+        $errors = [];
+
+        // WIP test à faire
+
+        require_once "config_session.inc.php";
+        if (!empty($errors)) {
+            $_SESSION["errorsSignup"] = $errors;
+            header("location: ../index.php");
+        }
+
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+
+} else {
+    header("location: ../index.php");
+    die();
 }
