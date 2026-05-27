12f22ed6458b0e7728f05fa4bff01064e099aeb6
kirian <kirian.jourand@gmail.com>
2026-05-27 14:45:24 +0200

    ✨ : Connexion à la BDD

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -9,8 +9,11 @@ define("DB_NAME", $_ENV['DB_NAME']);
 define("DB_USER", $_ENV['DB_USER']);
 define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
 
-$dbHost = $_ENV['DB_HOST'];
-$dbPort = $_ENV['DB_PORT'];
-$dbName = $_ENV['DB_NAME'];
-$dbUser = $_ENV['DB_USER'];
-$dbPassword = $_ENV['DB_PASSWORD'];
+
+// Connexion à la BDD
+try {
+    $pdo = new PDO("mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
