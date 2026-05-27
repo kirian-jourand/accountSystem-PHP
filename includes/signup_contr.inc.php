8f71ef4320086934e806438fe5364e4eba567a94
kirian <kirian.jourand@gmail.com>
2026-05-27 16:48:58 +0200

    ✨ : fonction pour test si l'e-mail est valide

--- includes/signup_contr.inc.php
+++ includes/signup_contr.inc.php
@@ -5,3 +5,6 @@ function isInputEmpty (string $username, string $pwd, string $email): bool {
     return empty($username) || empty($pwd) || empty($email);
 }
 
+function isEmailInvalid(string $email): bool {
+    return !filter_var($email, FILTER_VALIDATE_EMAIL);
+}
