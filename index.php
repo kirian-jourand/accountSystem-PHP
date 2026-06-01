1bf7167e68901747ee774132ed3b6d79d178e89a
kirian <kirian.jourand@gmail.com>
2026-06-01 13:20:54 +0200

    🥅💄 : Test si un champ est vide

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
     transition-all
     duration-800
     ease-out
+    w-sm
 ">
     <div class="grid grid-cols-1 gap-6">
         <h3 class=" text-2xl font-bold tracking-wide uppercase text-center">Login</h3>
@@ -63,42 +64,54 @@
 
     <div class="grid grid-cols-1 gap-6">
         <h3 class=" text-2xl font-bold tracking-wide uppercase text-center">Signup</h3>
-        <form method="POST" action="includes/signup.inc.php" class="grid grid-cols-1 gap-6">
-            <label for="username"> <span class="hidden">Username</span>
-                <input type="text" name="username" placeholder="Username" class="border border-white/25 rounded-2xl px-4 py-1 placeholder-gray-300 focus:outline-none focus:border-white w-full">
-            </label>
-            <label for="e-mail"> <span class="hidden">E-mail</span>
-                <input type="email" name="e-mail" placeholder="E-mail" class="border border-white/25 rounded-2xl px-4 py-1 placeholder-gray-300 focus:outline-none focus:border-white w-full">
-            </label>
-            <div id="signupPwdInput" class="mb-1 border border-white/25 rounded-2xl pl-4 py-1 placeholder-gray-300 flex items-center justify-center">
-                <label for="pwd"> <span class="hidden">Password</span>
-                    <input type="password" name="pwd" id="signupPassword" placeholder="Password" class="placeholder-gray-300 focus:outline-none focus:ring-0">
+        <form method="POST" action="includes/signup.inc.php" id="signup-form" class="grid grid-cols-1 gap-6">
+            <div class="field-wrapper">
+                <label for="username"> <span class="hidden">Username</span>
+                    <input type="text" name="username" placeholder="Username" class="border border-white/25 rounded-2xl px-4 py-1 placeholder-gray-300 focus:outline-none focus:border-white w-full">
                 </label>
-                <button type="button" id="signupTogglePwd" class="text-gray-300 w-12 flex justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="signupShowPwdIcon" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="signupHidePwdIcon" class="size-6 hidden">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                    </svg>
-                </button>
+                <span class="error-msg hidden  text-red-400 text-sm font-bold">Ce champ doit être rempli</span>
             </div>
-            <div id="signupConfirmPwdInput" class="mb-1 border border-white/25 rounded-2xl pl-4 py-1 placeholder-gray-300 flex items-center justify-center">
-                <label for="confirmPwd"> <span class="hidden">Confirm password</span>
-                    <input type="password" name="confirmPwd" id="signupConfirmPassword" placeholder="Confirm password" class="placeholder-gray-300 focus:outline-none focus:ring-0">
+            <div class="field-wrapper">
+                <label for="e-mail"> <span class="hidden">E-mail</span>
+                    <input type="email" name="e-mail" placeholder="E-mail" class="border border-white/25 rounded-2xl px-4 py-1 placeholder-gray-300 focus:outline-none focus:border-white w-full">
                 </label>
-                <button type="button" id="signupConfirmTogglePwd" class="text-gray-300 w-12 flex justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="signupConfirmShowPwdIcon" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="signupConfirmHidePwdIcon" class="size-6 hidden">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                    </svg>
-                </button>
+                <span class="error-msg hidden  text-red-400 text-sm font-bold">Ce champ doit être rempli</span>
             </div>
-            <button type="submit" class="bt-primary px-8 py-2 rounded-2xl uppercase text-md font-bold shadow-lg shadow-pink-600/50 hover:shadow-orange-300 transition-all duration-450 ease-in-out">Signup</button>
+            <div class="field-wrapper">
+                <div id="signupPwdInput" class="mb-1 border border-white/25 rounded-2xl py-1 placeholder-gray-300 flex items-center justify-center">
+                    <label for="pwd" class="grow"> <span class="hidden">Password</span>
+                        <input type="password" name="pwd" id="signupPassword" placeholder="Password" class="composed-field placeholder-gray-300 pl-4 focus:outline-none focus:ring-0">
+                    </label>
+                    <button type="button" id="signupTogglePwd" class="text-gray-300 w-12 flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="signupShowPwdIcon" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="signupHidePwdIcon" class="size-6 hidden">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                        </svg>
+                    </button>
+                </div>
+                <span class="error-msg hidden  text-red-400 text-sm font-bold">Ce champ doit être rempli</span>
+            </div>
+            <div class="field-wrapper">
+                <div id="signupConfirmPwdInput" class="mb-1 border border-white/25 rounded-2xl py-1 placeholder-gray-300 flex items-center justify-center">
+                    <label for="confirmPwd" class="grow"> <span class="hidden">Confirm password</span>
+                        <input type="password" name="confirmPwd" id="signupConfirmPassword" placeholder="Confirm password" class="composed-field placeholder-gray-300 pl-4 focus:outline-none focus:ring-0">
+                    </label>
+                    <button type="button" id="signupConfirmTogglePwd" class="text-gray-300 w-12 flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="signupConfirmShowPwdIcon" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" id="signupConfirmHidePwdIcon" class="size-6 hidden">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                        </svg>
+                    </button>
+                </div>
+                <span class="error-msg hidden  text-red-400 text-sm font-bold">Ce champ doit être rempli</span>
+            </div>
+            <button type="button" id="signup-submit-btn" class="bt-primary px-8 py-2 rounded-2xl uppercase text-md font-bold shadow-lg shadow-pink-600/50 hover:shadow-orange-300 transition-all duration-450 ease-in-out">Signup</button>
         </form>
     </div>
 </main>
@@ -170,7 +183,11 @@
 
     signupPassword.addEventListener('blur', () => {
         signupPwdInput.classList.remove("border-white");
-        signupPwdInput.classList.add("border-white/25");
+        if (signupPassword.classList.contains("error-state")) {
+            signupPwdInput.classList.add("border-red-400");
+        } else {
+            signupPwdInput.classList.add("border-white/25");
+        }
     });
 
     // logic for the signup  confirm password input
@@ -194,12 +211,83 @@
     }
 
     signupConfirmPassword.addEventListener("focus", () => {
-        signupConfirmPwdInput.classList.remove("border-white/25")
-        signupConfirmPwdInput.classList.add("border-white")
+        signupConfirmPwdInput.classList.remove("border-white/25");
+        signupConfirmPwdInput.classList.add("border-white");
     })
 
     signupConfirmPassword.addEventListener('blur', () => {
         signupConfirmPwdInput.classList.remove("border-white");
+        if (signupConfirmPassword.classList.contains("error-state")) {
+            signupConfirmPwdInput.classList.add("border-red-400");
+        } else {
+            signupConfirmPwdInput.classList.add("border-white/25");
+        }
+    });
+
+
+
+    const signupForm = document.getElementById("signup-form");
+    const signupSubmitBtn = document.getElementById("signup-submit-btn");
+
+    function checkEmptyFields(form) {
+        const fields = form.querySelectorAll('input');
+        console.log(fields);
+        let isValid = true;
+
+        fields.forEach(field => {
+            const wrapper  = field.closest('.field-wrapper');
+            const errorMsg = wrapper.querySelector('.error-msg');
+
+            if (field.value.trim() === "") {
+                field.classList.add("error-state");
+                // Application de la border rouge différente si c'est un input classique ou un input composé
+                // (ex. input password + bouton pour afficher/cacher)
+                if (field.classList.contains("composed-field")) {
+                    field.closest("div").classList.remove("border-white/25");
+                    field.closest("div").classList.add("border-red-400");
+                } else {
+                    field.classList.remove("border-white/25");
+                    field.classList.add("border-red-400");
+                }
+                errorMsg.classList.remove("hidden");
+                isValid = false;
+            } else {
+                field.classList.remove("error-state");
+
+                field.classList.remove("border-red-400");
+                field.classList.add("border-white/25");
+                errorMsg.classList.add("hidden");
+            }
+        });
+
+        return isValid;
+    }
+
+    function validateSignupForm () {
+        let isValid = true;
+        isValid = checkEmptyFields(signupForm);
+        return isValid;
+    }
+
+    signupSubmitBtn.addEventListener("click", () => {
+        if (validateSignupForm()) {
+            signupForm.submit();
+        }
+    });
+
+    // Retire l'erreur en temps réel quand l'utilisateur commence à taper
+    signupForm.querySelectorAll("input").forEach(field => {
+        field.addEventListener("input", () => {
+            const wrapper  = field.closest('.field-wrapper');
+            const errorMsg = wrapper.querySelector('.error-msg');
+
+            if (field.value.trim() !== '') {
+                field.classList.remove("error-state");
+                field.classList.remove("border-red-500");
+                field.classList.add("border-white/25")
+                errorMsg.classList.add('hidden');
+            }
+        });
         signupConfirmPwdInput.classList.add("border-white/25");
     });
 
