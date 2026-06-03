3015039694901e73763664927848b1a856175f81
kirian <kirian.jourand@gmail.com>
2026-06-03 11:55:35 +0200

    🎨 : test si les 2 variables de session sont bien set

--- includes/signup_view.inc.php
+++ includes/signup_view.inc.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 function getSignupSubmitFeedback(): array {
-    if (isset($_SESSION["errorsSignup"])) {
+    if (isset($_SESSION["errorsSignup"]) && isset($_SESSION["submitData"])) {
         $errors = $_SESSION["errorsSignup"];
         unset($_SESSION["errorsSignup"]);
         $submitData = $_SESSION["submitData"];
